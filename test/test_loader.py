fe89625ea2545f1a91a14f6a880af00634248063
Trevor McCasland <trevormccasland1@gmail.com>
2022-06-30 16:44:02 -0500

    add loading from disk examples

--- test/test_loader.py
+++ test/test_loader.py
@@ -1,8 +1,7 @@
 import os
 
-# The module that build_tests comes from.
 from gabbi import driver
-
+from gabbi.handlers import yaml_disk_loading_jsonhandler
 # By convention the YAML files are put in a directory named
 # "gabbits" that is in the same directory as the Python test file.
 TESTS_DIR = 'gabbits'
@@ -13,4 +12,5 @@ def load_tests(loader, tests, pattern):
     test_dir = os.path.join(os.path.dirname(__file__), TESTS_DIR)
     # Pass "require_ssl=True" as an argument to force all tests
     # to use SSL in requests.
-    return driver.build_tests(test_dir, loader, url='127.0.0.1:3500')
+    return driver.build_tests(test_dir, loader, url='127.0.0.1:3500',
+                              content_handlers=[yaml_disk_loading_jsonhandler.YAMLDiskLoadingJSONHandler])
